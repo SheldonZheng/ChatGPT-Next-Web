bbeb44442c7491bbf30cb795710fb10cb44eee2b
zhenghang <zhenghang@cmcm.com>
2023-08-08 18:04:54 +0800

    Modify title

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -470,9 +470,9 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>ChatGPT Next Web</div>
+            <div className={styles["main-title"]}>欧冶留学</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+              google.com
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
